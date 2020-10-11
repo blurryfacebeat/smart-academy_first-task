6cfc20e103d36d067d388a0d20fbbe9a4a59115e
blurryfacebeat <blurryfacebeat@gmail.com>
2020-10-11 19:23:40 +0700

    add adaptive, blocks layout

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Pricing Switcher
 var $pricing_switch = document.querySelector('.pricing-plan__switch-wrapper');
 var $pricing_switch_btns = document.querySelectorAll('.pricing-plan__switch__btn');
 var $value_standard = document.querySelector('#item-value-standard');
@@ -9,14 +10,14 @@ var value_premium = '49.00';
 $value_standard.innerHTML = value_standard;
 $value_premium.innerHTML = value_premium;
 $pricing_switch.addEventListener('click', function (e) {
-  var target = e.target;
-  pricingSwitchDisableActive($pricing_switch_btns); // if (target && target.classList.contains === 'pricing-plan__switch__btn_yearly') {
+  var target = e.target; // if (target && target.classList.contains === 'pricing-plan__switch__btn_yearly') {
   //     console.log(11);
   //     $value_standard.innerHTML = +value_standard * 12;
   //     $value_premium.innerHTML = +value_premium * 12;
   // } 
 
   if (target && target.classList.contains !== 'pricing-plan__switch__btn_active') {
+    pricingSwitchDisableActive($pricing_switch_btns);
     target.classList.add('pricing-plan__switch__btn_active');
   } // if (target && target.classList.contains === 'pricing-plan__switch__btn_monthly') {
   //     $value_standard.innerHTML = value_standard.toString;
@@ -28,5 +29,35 @@ $pricing_switch.addEventListener('click', function (e) {
 function pricingSwitchDisableActive(switch_btns) {
   switch_btns.forEach(function (el) {
     el.classList.remove('pricing-plan__switch__btn_active');
+  });
+} // Subscribe Form
+
+
+var $subscribe_form = document.querySelector('.subscribe form');
+var $subscribe_submit_btn = document.querySelector('.subscribe button');
+$subscribe_submit_btn.addEventListener('click', function (e) {
+  e.preventDefault();
+  $subscribe_form.reset();
+}); // Slider
+
+var $trust_items = document.querySelector('.trust__items');
+var $trust_item = document.querySelectorAll('.trust__item');
+var $slider_switcher = document.querySelector('.trust__slider__switcher');
+var $switcher_indicators = document.querySelectorAll('.slider__switcher__item');
+$slider_switcher.addEventListener('click', function (e) {
+  var target = e.target; // && target.classList.contains === 'slider__switcher__item'
+
+  if (target && target.classList.contains !== 'slider__switcher__item_active') {
+    sliderSwitcherDisabelActive($switcher_indicators);
+    target.classList.add('slider__switcher__item_active');
+  }
+}); // function translateSlides() {
+//     for (let i = 0; i < $trust_item.length; i++) {
+//     }
+// }
+
+function sliderSwitcherDisabelActive(switch_indicators) {
+  switch_indicators.forEach(function (el) {
+    el.classList.remove('slider__switcher__item_active');
   });
 }
--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+// Pricing Switcher
+
 const $pricing_switch = document.querySelector('.pricing-plan__switch-wrapper');
 const $pricing_switch_btns = document.querySelectorAll('.pricing-plan__switch__btn');
 const $value_standard = document.querySelector('#item-value-standard');
@@ -8,10 +10,8 @@ const value_premium = '49.00';
 $value_standard.innerHTML = value_standard;
 $value_premium.innerHTML = value_premium;
 
-$pricing_switch.addEventListener('click', function(e) {
+$pricing_switch.addEventListener('click', (e) => {
     const target = e.target;
-    
-    pricingSwitchDisableActive($pricing_switch_btns);
 
     // if (target && target.classList.contains === 'pricing-plan__switch__btn_yearly') {
     //     console.log(11);
@@ -20,6 +20,7 @@ $pricing_switch.addEventListener('click', function(e) {
     // } 
 
     if (target && target.classList.contains !== 'pricing-plan__switch__btn_active') {
+        pricingSwitchDisableActive($pricing_switch_btns);
         target.classList.add('pricing-plan__switch__btn_active');
     }
 
@@ -29,8 +30,50 @@ $pricing_switch.addEventListener('click', function(e) {
     // }
 });
 
+
 function pricingSwitchDisableActive(switch_btns) {
     switch_btns.forEach(el => {
         el.classList.remove('pricing-plan__switch__btn_active');
+    });
+}
+
+// Subscribe Form
+
+const $subscribe_form = document.querySelector('.subscribe form');
+const $subscribe_submit_btn = document.querySelector('.subscribe button');
+
+$subscribe_submit_btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    $subscribe_form.reset();
+});
+
+// Slider
+
+const $trust_items = document.querySelector('.trust__items');
+const $trust_item = document.querySelectorAll('.trust__item');
+const $slider_switcher = document.querySelector('.trust__slider__switcher');
+const $switcher_indicators = document.querySelectorAll('.slider__switcher__item');
+
+$slider_switcher.addEventListener('click', (e) => {
+    const target = e.target;
+
+    // && target.classList.contains === 'slider__switcher__item'
+    
+    if (target && target.classList.contains !== 'slider__switcher__item_active') {
+        sliderSwitcherDisabelActive($switcher_indicators);
+
+        target.classList.add('slider__switcher__item_active');
+    }
+});
+
+// function translateSlides() {
+//     for (let i = 0; i < $trust_item.length; i++) {
+
+//     }
+// }
+
+function sliderSwitcherDisabelActive(switch_indicators) {
+    switch_indicators.forEach(el => {
+        el.classList.remove('slider__switcher__item_active');
     });
 }
